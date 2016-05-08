180cfd79272a80108684284038bfbf60bbe2b008
Agung Suyono <agungsuyono@gmail.com>
2016-05-08 20:25:00 +0700

    fixing library definition

--- src/Controller/DrupalNgController.php
+++ src/Controller/DrupalNgController.php
@@ -1,31 +1,10 @@
 <?php
-
-/**
- * @file
- * Contains \Drupal\drupal_ng\Controller\BscModelController.
- */
-
 namespace Drupal\drupal_ng\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-/**
- * Controller for js_example pages.
- *
- * @ingroup js_example
- */
 class DrupalNgController extends ControllerBase {
 
-  /**
-   * Accordion page implementation.
-   *
-   * We're allowing a twig template to define our content in this case,
-   * which isn't normally how things work, but it's easier to demonstrate
-   * the JavaScript this way.
-   *
-   * @return array
-   *   A renderable array.
-   */
   public function viewDrupalNg() {
     $title = t('Hello!');
     $build['myelement'] = array(
@@ -33,9 +12,7 @@ class DrupalNgController extends ControllerBase {
       '#title' => $title,
     );
     $build['myelement']['#attached']['library'][] = 'drupal_ng/angularjs';
-    $build['myelement']['#attached']['library'][] = 'bsc_model/model';
-    // Return the renderable array.
+    $build['myelement']['#attached']['library'][] = 'drupal_ng/drupal_ng';
     return $build;
   }
-
 }
